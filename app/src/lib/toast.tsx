3e987ae811638761489ffb795a5f261bfc362b98
Aamna Ashraf <159810000+aamnaashraf@users.noreply.github.com>
2025-12-30 01:16:21 +0500

    Update toast.tsx

--- app/src/lib/toast.tsx
+++ app/src/lib/toast.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+
 import { createContext, useContext, useState, ReactNode } from 'react'
-import Toast from '@/src/components/ui/Toast'
+import Toast from '@/components/ui/Toast'
 
 type ToastType = 'success' | 'error' | 'info' | 'warning'
 
